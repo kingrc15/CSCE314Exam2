5b11bfa6caefa954595933dce25561f2cf9daf28
Cade Conner <55031759+realcadecon@users.noreply.github.com>
2020-04-28 23:13:51 -0500

    Question 2 for Collections Lab

--- JavaCollections/src/Animal.java
+++ JavaCollections/src/Animal.java
@@ -1,5 +1,98 @@
+import java.util.*;
 
-public class Animal
-{
+public class Animal extends Address implements Comparable<Animal>{
+	private int type;
+	private String foodRequired;
+	private float weight;
+	
+	private String state; // 2 chars only!!
+	private String street;
+	private int zip;
+	private String town;
+	
+	
+	
+	public Animal(int type, String foodRequired, float weight, String state, String street, int zip, String town) {
+		super(state, street, zip, town);
+		this.type = type;
+		this.foodRequired = foodRequired;
+		this.weight = weight;
+	}
+	
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public String getFoodRequired() {
+		return foodRequired;
+	}
+	public void setFoodRequired(String foodRequired) {
+		this.foodRequired = foodRequired;
+	}
+	public float getWeight() {
+		return weight;
+	}
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Animal other = (Animal) obj;
+		if (foodRequired == null) {
+			if (other.foodRequired != null)
+				return false;
+		} else if (!foodRequired.equals(other.foodRequired))
+			return false;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		if (street == null) {
+			if (other.street != null)
+				return false;
+		} else if (!street.equals(other.street))
+			return false;
+		if (town == null) {
+			if (other.town != null)
+				return false;
+		} else if (!town.equals(other.town))
+			return false;
+		if (type != other.type)
+			return false;
+		if (Float.floatToIntBits(weight) != Float.floatToIntBits(other.weight))
+			return false;
+		if (zip != other.zip)
+			return false;
+		return true;
+	}
+	
+	public int compareTo(Animal x) {
+		if(this.getWeight() > x.getWeight()) {
+			return 1;
+		}
+		else if (this.getWeight() < x.getWeight()) {
+			return -1;
+		}
+		return 0;
+	}
 
+	@Override
+	public String toString() {
+		return "Animal [type=" + type + ", foodRequired=" + foodRequired + ", weight=" + weight + "]";
+	}
+	
+	
+		
+	
+	
 }
--- JavaCollections/src/Driver.java
+++ JavaCollections/src/Driver.java
@@ -1,11 +1,61 @@
+import java.util.*;
 
-public class Driver
-{
+public class Driver {
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Vehicle car4 = new Vehicle(0, 20, "LICENSE4", 4f, "Texas", "123 Street Ln", 77840, "College Station");
+		Vehicle car3 = new Vehicle(0, 20, "LICENSE3", 3f, "Texas", "123 Street Ln", 77840, "College Station");
+		Vehicle car2 = new Vehicle(0, 20, "LICENSE2", 2f,"Texas", "123 Street Ln", 77840, "College Station");
+		Vehicle car1 = new Vehicle(0, 20, "LICENSE1", 1f, "Texas", "123 Street Ln", 77840, "College Station");
+		
+		Animal dog4 = new Animal(0, "Dog Food", 4f, "Texas", "123 Street Ln", 77840, "College Station");
+		Animal dog3 = new Animal(0, "Dog Food", 3f, "Texas", "123 Street Ln", 77840, "College Station");
+		Animal dog2 = new Animal(0, "Dog Food", 2f, "Texas", "123 Street Ln", 77840, "College Station");
+		Animal dog1 = new Animal(0, "Dog Food", 1f, "Texas", "123 Street Ln", 77840, "College Station");
+		
+		ArrayList<Animal> animalList = new ArrayList<Animal>();
+		ArrayList<Vehicle> vehicleList = new ArrayList<Vehicle>();
+		
+		vehicleList.add(car4);
+		vehicleList.add(car3);
+		vehicleList.add(car2);
+		vehicleList.add(car1);
+		
+		animalList.add(dog4);
+		animalList.add(dog3);
+		animalList.add(dog2);
+		animalList.add(dog1);
+	
+		System.out.println(animalList);
+		Collections.sort(animalList);
+		System.out.println(animalList);
+		
+		System.out.println("\n");
+		
+		System.out.println(vehicleList);
+		Collections.sort(vehicleList);
+		System.out.println(vehicleList);
+		
+		
+		System.out.println("\n");
+		ArrayList<Object> stuff = new ArrayList<Object>();
+		stuff.add(car4);
+		stuff.add(car3);
+		stuff.add(car2);
+		stuff.add(car1);
+		
+		stuff.add(dog4);
+		stuff.add(dog3);
+		stuff.add(dog2);
+		stuff.add(dog1);
+		
+		for(int i=0; i<stuff.size(); i++) {
+			System.out.println(stuff.get(i));
+		}
+		
+		
+		
 	}
 
 }
